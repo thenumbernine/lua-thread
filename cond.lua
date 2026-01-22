cdbef10110d54e5f37ede4c669d393c5715b1018
Christopher Moore <christopher.e.moore@gmail.com>
2026-01-22 18:34:43 +0700

    added condition variables

---
+++ cond.lua
@@ -0,0 +1,49 @@
+require 'ext.gc'
+local ffi = require 'ffi'
+local class = require 'ext.class'
+local pthread = require 'ffi.req' 'c.pthread'	-- pthread_mutex_t
+local thread_assert = require 'thread.assert'
+
+
+local pthread_cond_t_1 = ffi.typeof'pthread_cond_t[1]'
+
+
+local Cond = class()
+
+function Cond:init()
+	self.id = pthread_cond_t_1()
+	thread_assert(pthread.pthread_cond_init(self.id, nil), 'pthread_cond_init')
+end
+
+function Cond:destroy()
+	if not self.id then return true end
+	local err = pthread.pthread_mutex_destroy(self.id)
+	if err == 0 then self.id = nil end
+	return 0 == err, err
+end
+
+function Cond:__gc()
+	self:destroy()
+end
+
+function Cond:signal()
+	local err = pthread.pthread_cond_signal(self.id)
+	return 0 == err, err
+end
+
+function Cond:broadcast()
+	local err = pthread.pthread_cond_broadcast(self.id)
+	return 0 == err, err
+end
+
+function Cond:wait(mutex)
+	local err = pthread.pthread_cond_wait(self.id, mutex.id)
+	return 0 == err, err
+end
+
+function Cond:timedwait(mutex, abstime)
+	local err = pthread.pthread_cond_timedwait(self.id, mutex.id, abstime)
+	return 0 == err, err
+end
+
+return Cond
